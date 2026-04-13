3a39d14fcbda2342c3e4037f5b0ab32b86d422e2
Nikhilreddy1867 <Nikhilreddy1867@users.noreply.github.com>
2026-04-14 00:30:22 +0530

    today dp and min max dp

--- lc24.java
+++ lc24.java
@@ -1,3 +1,27 @@
-public class lc24 {
-    
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int sum=nums[0]+nums[1]+nums[2];
+        for(int k=0;k<n;k++){
+            int i=k+1;
+            int j=n-1;
+            while(i<j){
+                int currSum = nums[i] + nums[j] + nums[k]; 
+                if(Math.abs(target-currSum)<Math.abs(target-sum)){
+                    sum=currSum;
+                }
+                if(currSum<target){
+                    i++;
+                }
+                else if(currSum>target){
+                    j--;
+                }
+                else{
+                    return target;
+                }
+            }
+        }
+        return sum;
+    }
 }
